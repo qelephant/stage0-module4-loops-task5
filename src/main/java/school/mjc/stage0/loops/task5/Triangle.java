1869b2b9014cecc5a695b7a9134f5486437ded3c
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-05 20:39:56 +0600

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int row = 1; row <= cathetusLength; row++) {
+            for (int col = 1; col <= row && col <= cathetusLength; col++) {
+            System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
