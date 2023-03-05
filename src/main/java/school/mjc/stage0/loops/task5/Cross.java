3293485835e1cb41fc01941c69676f6b3f5b949d
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-05 19:50:39 +0600

    Update Cross.java

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int row = 1; row <= sideLength; row++) {
+            for (int col = 1; col <= sideLength; col++) {
+                if (row == col || row == sideLength - col + 1) {
+                System.out.print("8");
+                } else {
+                System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
