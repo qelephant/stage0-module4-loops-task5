fa010f11c15e4062a3debf7d7abd6df40bf719be
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-03-05 20:57:27 +0600

    Update Rectangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -3,8 +3,8 @@ package school.mjc.stage0.loops.task5;
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
         for (int row = 1; row <= length; row++) {
-            for (int col = 1; col <= width; col++) {
-                if (row == 1 || row == length || col == 1 || col == width) {
+            for (int col = 1; col <= height; col++) {
+                if (row == 1 || row == length || col == 1 || col == height) {
                     System.out.print("8");
                 } else {
                     System.out.print(" ");
